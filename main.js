2eac232714cbe0a75278e458770dd14ac03f4fab
Simone Hoffmann <simohoff@gmx.de>
2020-11-22 18:00:09 +0100

    added images and work page

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ console.log('Hello from main.js!')
 // get li tag for work
 const work = document.getElementById("work")
 
-
-console.log(work);
 var onButtonClick = function() {
     console.log('henlo');
+
+    // hide home page and menu footer
+    document.querySelector('.home.page.show').classList.remove("show")
+    document.querySelector('.footer-menu.show').classList.remove("show") 
+    
+    // show work page
+    document.querySelector('.work.page.hide').classList.remove("hide")
+    document.querySelector('.work.page').classList.add("show")
+
+    
 };
 // 3. Add the event listener for the element and function
 work.addEventListener("click", onButtonClick);
